96092e84a152f0d3e78c4b361561f3f248ddbd5e
ThomasFoydel <thomasjfoydel@gmail.com>
2021-06-18 08:06:47 -0500

    spring animation fade user profile

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -5,6 +5,8 @@ import styles from '../../styles/Post.module.css';
 import { useSession } from 'next-auth/client';
 import Link from 'next/link';
 import Image from '../../components/Image';
+import { animated, useSpring, config } from 'react-spring';
+
 const Post = () => {
   const router = useRouter();
   const [post, setPost] = useState(null);
@@ -50,37 +52,50 @@ const Post = () => {
 };
 const PostDisplay = ({
   props: { post, submitComment, deletePost, deleteComment, session },
-}) => (
-  <div className='blogpost'>
-    <h3>{post.title}</h3>
+}) => {
+  const animation = useSpring({
+    from: { opacity: 0, transform: 'translateY(-100px)' },
+    to: { opacity: 1, transform: 'translateY(0px)' },
+    config: config.molasses,
+  });
+  return (
+    <animated.div className='blogpost' style={animation}>
+      <h3>{post.title}</h3>
 
-    {session?.sub === post.author._id && (
-      <button onClick={() => deletePost(post._id)} className={styles.deleteBtn}>
-        X
-      </button>
-    )}
-    <Link href={`/profile/${post.author._id}`}>
-      <p className='author'>{post.author.name}</p>
-    </Link>
-    <time className='date'>
-      <span>{new Date(post.createdAt).toLocaleDateString()}, </span>
-      <span>{new Date(post.createdAt).toLocaleTimeString()}</span>
-    </time>
-    {post.imageUrl && (
-      <Image
-        src={post.imageUrl}
-        alt='post picture'
-        className='img'
-        fallbackSrc={'/logo.png'}
-      />
-    )}
-    <p className='content'>{post.content}</p>
-    {post.comments.map((comment) => (
-      <Comment props={{ comment, deleteComment, session }} key={comment._id} />
-    ))}
-    <CommentForm props={{ submitComment }} />
-  </div>
-);
+      {session?.sub === post.author._id && (
+        <button
+          onClick={() => deletePost(post._id)}
+          className={styles.deleteBtn}
+        >
+          X
+        </button>
+      )}
+      <Link href={`/profile/${post.author._id}`}>
+        <p className='author'>{post.author.name}</p>
+      </Link>
+      <time className='date'>
+        <span>{new Date(post.createdAt).toLocaleDateString()}, </span>
+        <span>{new Date(post.createdAt).toLocaleTimeString()}</span>
+      </time>
+      {post.imageUrl && (
+        <Image
+          src={post.imageUrl}
+          alt='post picture'
+          className='img'
+          fallbackSrc={'/logo.png'}
+        />
+      )}
+      <p className='content'>{post.content}</p>
+      {post.comments.map((comment) => (
+        <Comment
+          props={{ comment, deleteComment, session }}
+          key={comment._id}
+        />
+      ))}
+      <CommentForm props={{ submitComment }} />
+    </animated.div>
+  );
+};
 
 const CommentForm = ({ props: { submitComment } }) => {
   const [content, setContent] = useState('');
