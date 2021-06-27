7059d896f824463e0154a114064fd179c9a99ef1
ThomasFoydel <thomasjfoydel@gmail.com>
2021-06-26 19:16:04 -0500

    user profile usertransition spring animation, profile info and posts

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -4,7 +4,7 @@ import { useSession } from 'next-auth/client';
 import { useRouter } from 'next/router';
 import styles from '../../styles/Profile.module.css';
 import Link from 'next/link';
-import { animated, useSpring, config } from 'react-spring';
+import { animated, useTransition, config } from 'react-spring';
 
 const BlogPost = ({ props: { post } }) => (
   <div className='blogpost'>
@@ -23,7 +23,7 @@ export default function Profile() {
   const [posts, setPosts] = useState([]);
   const [session] = useSession();
   const [user, setUser] = useState({});
-  const [following, setFollowing] = useState(false);
+  const [following, setFollowing] = useState(true);
   const [ownProfile, setOwnProfile] = useState(false);
   const router = useRouter();
 
@@ -51,43 +51,52 @@ export default function Profile() {
       .then(({ data }) => console.log(data))
       .catch((err) => console.log(err));
 
-  const animation = useSpring({
-    from: { opacity: 0, transform: 'translateY(-100px)' },
-    to: { opacity: 1, transform: 'translateY(0px)' },
-    config: config.molasses,
+  const transition1 = useTransition([user, ...posts], (item) => item._id, {
+    from: { opacity: '0', transform: 'translateY(-100px)' },
+    enter: { opacity: '1', transform: 'translateY(0px)' },
+    config: config.smooth,
   });
 
   return (
     <div className={styles.profile}>
-      <animated.div style={animation}>
-        <h2>{user.name}</h2>
-        {user.profilePic && (
-          <div className={styles.imgContainer}>
-            <img
-              className={styles.profilePic}
-              src={user.profilePic}
-              alt={`${user.name}'s profile`}
-            />
-            <div className={styles.shade} />
-          </div>
-        )}
-        <div className={styles.links}>
-          {user.github && (
-            <a href={`https://github.com/${user.github}`} target='_blank'>
-              github
-            </a>
-          )}
-          {ownProfile && (
-            <Link href='/editprofile'>
-              <a>edit profile</a>
-            </Link>
-          )}
-          {!following && <button onClick={handleFollow}>follow</button>}
-        </div>
-      </animated.div>
-      {posts.map((post) => (
-        <BlogPost props={{ post }} key={post._id} />
-      ))}
+      {transition1.map(({ item, props, key }, i) => {
+        if (i === 0) {
+          return (
+            <animated.div style={props} key={key}>
+              <h2>{user.name}</h2>
+              {user.profilePic && (
+                <div className={styles.imgContainer}>
+                  <img
+                    className={styles.profilePic}
+                    src={user.profilePic}
+                    alt={`${user.name}'s profile`}
+                  />
+                  <div className={styles.shade} />
+                </div>
+              )}
+              <div className={styles.links}>
+                {user.github && (
+                  <a href={`https://github.com/${user.github}`} target='_blank'>
+                    github
+                  </a>
+                )}
+                {ownProfile && (
+                  <Link href='/editprofile'>
+                    <a>edit profile</a>
+                  </Link>
+                )}
+                {!following && <button onClick={handleFollow}>follow</button>}
+              </div>
+            </animated.div>
+          );
+        } else {
+          return (
+            <animated.div style={props} key={key}>
+              <BlogPost props={{ post: item }} />
+            </animated.div>
+          );
+        }
+      })}
     </div>
   );
 }
